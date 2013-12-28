dc5ef4182783440c74d6c0efa05a075108c4a7fa
Matthew Schulman <matthewdschulman@gmail.com>
2013-12-28 15:30:39 -0500

    Create Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,6 +34,17 @@ describe "StaticPages" do
 		end
 	end
 
+	describe "Contact page" do
+		it "should have the content 'contact' " do
+			vist '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end
+
 	
 
 end
