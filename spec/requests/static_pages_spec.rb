a7fb6991e89e85d25530fff6d2f45c769bb5eed5
Matthew Schulman <matthewdschulman@gmail.com>
2013-12-28 16:59:04 -0500

    Finish section 4.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 		it "should have the content 'Sample App' " do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
-		it "should have the right title" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-		end
-	end
+		it "should have the base title" do
+      		visit '/static_pages/home'
+      		expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    	end
+
+	    it "should not have a custom page title" do
+	      	visit '/static_pages/home'
+	      	expect(page).not_to have_title('| Home')
+	    end
+  	end
 
 	describe "Help page" do
 		it "should have the content 'Help' " do
@@ -19,7 +27,7 @@ describe "StaticPages" do
 		end
 		it "should have the right title" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -30,18 +38,18 @@ describe "StaticPages" do
 		end
 		it "should have the right title" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+			expect(page).to have_title("#{base_title} | About Us")
 		end
 	end
 
 	describe "Contact page" do
 		it "should have the content 'contact' " do
-			vist '/static_pages/contact'
-			expect(page).to have_content('Contact')
+			visit '/static_pages/contact'
+			expect(page).to have_content('contact')
 		end
 		it "should have the right title" do
 			visit '/static_pages/contact'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
 
