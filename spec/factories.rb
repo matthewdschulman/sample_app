8b1ec1b304040d0e4ba32392fbfa004fdb3ad2db
Matthew Schulman <matthewdschulman@gmail.com>
2014-01-11 22:51:35 -0500

    Finish user signup [ch7]

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name "Matthew Schulman"
+		email "matthewdschulman@gmail.com"
+		password "43ds9xstink"
+		password_confirmation "43ds9xstink"
+	end
+end
