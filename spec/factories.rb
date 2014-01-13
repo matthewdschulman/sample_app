1a6e4b416a2e5266574cf2cc07a0cb470117151e
Matthew Schulman <matthewdschulman@gmail.com>
2014-01-12 20:49:45 -0500

    Finish user edit, update, index, and destroy actions (ch 9)

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
-FactoryGirl.define do 
-	factory :user do
-		name "Matthew Schulman"
-		email "matthewdschulman@gmail.com"
-		password "43ds9xstink"
-		password_confirmation "43ds9xstink"
-	end
+FactoryGirl.define do
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,6 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
@@ -16,20 +15,17 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
   end
 end
